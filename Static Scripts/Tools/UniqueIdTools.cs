7e4d17b48cbcac66a01a08709f066df605e96e9e
taha <stmh1363@gmail.com>
2025-05-12 22:54:16 +0330

    Improved Unique Id Tool

--- Static Scripts/Tools/UniqueIdTools.cs
+++ Static Scripts/Tools/UniqueIdTools.cs
@@ -6,6 +6,7 @@ public static class UniqueIdTools
     {
         string xPart = iPosition.x.ToString("F2").Replace(".", "");
         string yPart = iPosition.y.ToString("F2").Replace(".", "");
+        string pPart = " ";
 
         if (xPart.Length > 6)
             xPart = xPart.Substring(0, 5);
@@ -16,7 +17,26 @@ public static class UniqueIdTools
         int currentScene;
         currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
 
-        return xPart + yPart + "_" + currentScene;
+        return xPart + yPart + pPart + "_" + currentScene;
+    }
+    public static string _MakeUniqueId(Vector2 iPosition , Transform iParent)
+    {
+        string xPart = iPosition.x.ToString("F2").Replace(".", "");
+        string yPart = iPosition.y.ToString("F2").Replace(".", "");
+        string pPart = iParent.name.Replace(".", "");
+
+        if (xPart.Length > 6)
+            xPart = xPart.Substring(0, 5);
+        if (yPart.Length > 6)
+            yPart = yPart.Substring(0, 5);
+        if (pPart.Length > 6)
+            pPart = pPart.Substring(0, 2) + pPart.Substring(pPart.Length - 2, pPart.Length);
+
+        // we add currentScene to ensure the id is unique in different scenes
+        int currentScene;
+        currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+
+        return xPart + yPart + pPart + "_" + currentScene;
     }
     public static int _GetUniqueIdScene(string iUniqueId)
     {
