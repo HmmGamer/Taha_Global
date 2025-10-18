d637e10592370d9746f8c22f4fc8234aa98caf46
taha <stmh1363@gmail.com>
2025-10-18 20:43:21 +0330

    fixed an small mistake in summaries

--- Attributes/Attr_AutoInvoke.cs
+++ Attributes/Attr_AutoInvoke.cs
@@ -3,11 +3,29 @@ using UnityEngine;
 using UnityEditor;
 using System.Reflection;
 using System.Collections.Generic;
-using CustomAttributes;
 
-namespace CustomAttributes
-{
-    // The AutoInvoke attribute
+/// <summary>
+/// Caution : this script is not optimized and completely tested yet
+/// 
+/// Automatically invokes a specified method when the field's value changes in the Inspector.
+/// Attach this to fields in a MonoBehaviour. The target method must be parameterless.
+/// The method is called whenever the field value changes or after play mode ends if the value changed.
+/// </summary>
+
+//class _AutoInvokeSample : MonoBehaviour
+//{
+//    [AutoInvoke("OnValueChanged")]
+//    public int _myValue;
+
+//    [AutoInvoke("OnValueChanged")]
+//    public string _myString;
+
+//    private void OnValueChanged()
+//    {
+//        Debug.Log($"Value changed: {_myValue}, {_myString}");
+//    }
+//}
+
     [AttributeUsage(AttributeTargets.Field)]
     public class AutoInvokeAttribute : PropertyAttribute
     {
@@ -203,4 +221,3 @@ namespace CustomAttributes
         }
     }
 #endif
-}
