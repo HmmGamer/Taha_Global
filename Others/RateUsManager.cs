f0ff3411f19d739e03bcb20473893146e3860261
taha <stmh1363@gmail.com>
2025-11-30 22:07:31 +0330

    added intents - incomplete

---
+++ Others/RateUsManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RateUsManager : MonoBehaviour
+{
+    [SerializeField] int[] _showCd;
+    [SerializeField] MessageBoxController _msgBox;
+
+    bool _hasShownRateUs;
+
+    private void Start()
+    {
+        _LoadData();
+        _CheckShowRateUs();
+    }
+    private void _CheckShowRateUs()
+    {
+        if (_hasShownRateUs)
+            if (LoadingTimesManager._instance._HasReachedNumber(_showCd))
+                _ShowRateUs_WithConfirmation();
+    }
+    private void _ShowRateUs_WithConfirmation()
+    {
+        UnityEvent _event = new UnityEvent();
+        _event.AddListener(() => IntentManager._instance._OpenIntent(_Intents.RateUs));
+        _event.AddListener(_SaveHasShownRateUs);
+
+        _msgBox._AskForConfirmation(_event);
+    }
+    private void _SaveHasShownRateUs()
+    {
+        A.DataKey._SetTrue(A.DataKey.hasShownRateUs);
+    }
+    private void _LoadData()
+    {
+        _hasShownRateUs = A.DataKey._IsTrue(A.DataKey.hasShownRateUs);
+    }
+}
